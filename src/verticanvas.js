fd62d34a9e8e978f3c77025348043727bd595d9f
22century <admin@22century.coresv.net>
2013-07-04 02:21:51 +0900

    超途中

---
+++ src/verticanvas.js
@@ -0,0 +1,541 @@
+/**
+ * VertiCanvas（仮称）
+ * Canvas縦書き組版ライブラリー
+ *
+ * @auther 22century
+ * @license MIT license
+ * @version 0.1
+ */
+
+(function(window){
+
+    /**
+     * VertiCanvas
+     * @param {object} options
+     * @constructor
+     */
+    var VertiCanvas = function(options){
+
+        for (var protoName in VertiCanvas.prototype) {
+            if (VertiCanvas.prototype.hasOwnProperty(protoName)) {
+                if (options.hasOwnProperty(protoName)
+                && protoName.indexOf("_") !== 0
+                && typeof options[protoName] !== "function"
+                ) {
+                    this[protoName] = options[protoName];
+                }
+            }
+        }
+
+        this.initialize();
+    };
+
+    VertiCanvas.prototype = {
+
+        text            : "",
+        _charAry         : [],
+        _imageCache      : {},
+
+        _lastPoint       : 0,
+
+        width           : 100,
+        height          : 100,
+
+        mainCanvasId    : "reader",
+        _mainCanvas      : null,
+        _mainContext     : null,
+
+        _workCanvasId    : "work",
+        _workCanvas      : null,
+        _workContext     : null,
+
+        fontSize        : 16,
+        fontFamily      : "serif",
+        _fontStyle       : "",
+        _letterSpacing   : "",
+        lineHeight      : 10,
+
+        _patternSpace    : new RegExp("[\t ]"),
+        _patternNewline  : new RegExp("[\r\n]"),
+        _patternAlphaNum : new RegExp("[a-zA-Z0-9]"),
+
+        _debug: true,
+
+        /**
+         * initialize
+         */
+        initialize: function(){
+
+            console.log("initialize:");
+
+            var canvas = document.getElementById(this._workCanvasId);
+            if (!canvas || !canvas.getContext) {
+                return;
+            }
+
+            this._fontStyle = ["normal", this.fontSize + "px", this.fontFamily].join(" ");
+            this.setupCanvas();
+            this.text = this.replaceLineBreak(this.text);
+            this._charAry = this.getCharArray(this.text);
+            this.renderCanvas(this._charAry);
+
+            this.initialize = null;
+        },
+
+        nextPage: function(){
+            this.resetMainCanvas();
+            this.renderCanvas(this._charAry, this._lastPoint);
+        },
+
+        prevPage: function(){
+            this.resetMainCanvas();
+            this.renderCanvas(this._charAry, this._lastPoint);
+        },
+
+        getMarginMap: function(){
+            var map = [];
+            for (var i= 0,l=this.fontSize;i<l;i++) {
+                map[i] = 0;
+            }
+            return map;
+        },
+
+        /**
+         * CANVAS設定
+         */
+        setupCanvas: function(){
+
+            this._mainCanvas = document.getElementById(this.mainCanvasId);
+            this._mainCanvas.width = this.width;
+            this._mainCanvas.height = this.height;
+
+            this._mainContext = this._mainCanvas.getContext("2d");
+            this._mainContext.font = this._fontStyle;
+            this._mainContext.save();
+
+            this._workCanvas = document.getElementById(this._workCanvasId);
+            this._workCanvas.width = this.fontSize;
+            this._workCanvas.height = this.fontSize;
+
+            this._workContext = this._workCanvas.getContext("2d");
+            this._workContext.font = this._fontStyle;
+            this._workContext.save();
+
+        },
+
+        /**
+         * 改行コードを統一
+         * @param {string} str
+         * @returns {string}
+         */
+        replaceLineBreak: function(str){
+            return str.split(this._patternNewline).join("\n");
+        },
+
+        /**
+         * 一文字ずつの配列を作成（サロゲートペアは2つで1セット）
+         * @param {string} str
+         * @returns {Array}
+         */
+        getCharArray: function(str){
+
+            var charAry = [];
+            var hCharCode, lCharCode;
+
+            for (var i= 0,l=str.length; i<l; i++) {
+                hCharCode = str.charCodeAt(i);
+                // 上位サロゲート
+                if ((0xD800 <= hCharCode && hCharCode <= 0xDBFF)) {
+                    lCharCode = str.charCodeAt(i + 1);
+                    // 下位サロゲート
+                    if (0xDC00 <= lCharCode && lCharCode <= 0xDFFF) {
+                        charAry[charAry.length] = String.fromCharCode(hCharCode, lCharCode);
+                    }
+                    ++ i;
+                } else {
+                    charAry[charAry.length] = str[i];
+                }
+            }
+
+            return charAry;
+        },
+
+        /**
+         * 空白判定
+         * @param {string} str
+         * @returns {boolean}
+         */
+        isSpace: function(str){
+            return this._patternSpace.test(str);
+        },
+
+        /**
+         * 改行判定
+         * @param {string} str
+         * @returns {boolean}
+         */
+        isLineBreak: function(str){
+            return this._patternNewline.test(str);
+        },
+
+        /**
+         * 英数判定
+         * @param {string} str
+         * @returns {boolean}
+         */
+        isAlphaNum: function(str){
+            return this._patternAlphaNum.test(str);
+        },
+
+        /**
+         * 行頭禁則文字
+         * @param {string} str
+         * @returns {boolean}
+         */
+        isNotAllowedStart: function(str){
+            return "）。.,)]｝、〕〉》」』】〙〗〟’”｠»・:;/‐゠–〜～?!‼⁇⁈⁉".indexOf(str) !== -1;
+        },
+
+        /**
+         * 行末禁則文字
+         * @param {string} str
+         * @returns {boolean}
+         */
+        isNotAllowedEnd: function(str){
+            return "（([｛〔〈《「『【〘〖〝‘“｟«".indexOf(str) !== -1;
+        },
+
+        /**
+         * 始め括弧
+         * @param {string} str
+         * @returns {boolean}
+         */
+        isOpeningBracket: function(str){
+            return "（([｛〔〈《「『【〘〖〝‘“｟«".indexOf(str) !== -1;
+        },
+
+        /**
+         * 閉じ括弧
+         * @param {string} str
+         * @returns {boolean}
+         */
+        isClosingBracket: function(str){
+            return "）)]｝〕〉》」』】〙〗〟’”｠»".indexOf(str) !== -1;
+        },
+
+        /**
+         * 長音符
+         * @param {string} str
+         * @returns {boolean}
+         */
+        isHyphen: function(str){
+            return "ー‐゠–〜～".indexOf(str) !== -1;
+        },
+
+        /**
+         * 句読点
+         * @param {string} str
+         * @returns {boolean}
+         */
+        isPunctuation: function(str){
+            return "｡。．.、，".indexOf(str) !== -1;
+        },
+
+        /**
+         * メインCanvasの消去
+         */
+        resetMainCanvas: function(){
+            this._mainContext.setTransform(1, 0, 0, 1, 0, 0);
+            this._mainContext.clearRect(0, 0, this.width, this.height);
+        },
+
+        /**
+         * 作業用Canvasの消去
+         */
+        resetWorkCanvas: function(){
+            this._workContext.setTransform(1, 0, 0, 1, 0, 0);
+            this._workContext.clearRect(0, 0, 100, 100);
+        },
+
+        /**
+         * 英数字の縦書き
+         * @param {string} str
+         * @param {number} x
+         * @param {number} y
+         */
+        fillAlphaNum: function(str, x, y){
+            if (typeof this._imageCache[str] === "undefined") {
+                this._workContext.rotate(90/180 * Math.PI);
+                this._workContext.fillText(str, 0, 0);
+                this._imageCache[str] = this._workContext.getImageData(0, 0, this.fontSize, this.fontSize);
+                this.resetWorkCanvas();
+            }
+            this._mainContext.putImageData(this._imageCache[str], x + this.fontSize / 10, y - this.fontSize + this.fontSize/4);
+        },
+
+        /**
+         * 長音符の縦書き
+         * @param {string} str
+         * @param {number} x
+         * @param {number} y
+         */
+        fillHyphen: function(str, x, y){
+
+            var image;
+
+            //if (typeof this._imageCache[str] === "undefined") {
+                this._workContext.rotate(90 / 180 * Math.PI);
+                this._workContext.transform(1, 0, 0, -1, 0, 0);
+                this._workContext.fillText(str, 0, this.fontSize);
+
+                // プリレンダー
+                image = this._workContext.getImageData(0, 0, this.fontSize, this.fontSize);
+                var exData = this.getFillExtent(image);
+
+                // 最小範囲で再選択
+                image = this._workContext.getImageData(exData.left, 0, exData.originWidth, this.fontSize);
+
+                this.resetWorkCanvas();
+                this._mainContext.putImageData(
+                    image,
+                    x + exData.horizontalMargin,
+                    y + exData.verticalMargin - this.fontSize
+                );
+            //}
+
+            //image = this._imageCache[str];
+        },
+
+        /**
+         * 始め括弧の縦書き
+         * @param {string} str
+         * @param {number} x
+         * @param {number} y
+         */
+        fillOpeningBracket: function(str, x, y){
+            if (typeof this._imageCache[str] === "undefined") {
+                this._workContext.rotate(90/180 * Math.PI);
+                this._workContext.transform(1, 0, 0, -1, 0, 0);
+                this._workContext.fillText(str, 0 , this.fontSize - (this.fontSize/10));
+                this._imageCache[str] = this._workContext.getImageData(0, 0, this.fontSize, this.fontSize);
+                this.resetWorkCanvas();
+            }
+            this._mainContext.putImageData(this._imageCache[str] , x, y - this.fontSize - this.fontSize/10);
+        },
+
+        /**
+         * 閉じ括弧の縦書き
+         * @param {string} str
+         * @param {number} x
+         * @param {number} y
+         */
+        fillClosingBracket: function(str, x, y){
+            if (typeof this._imageCache[str] === "undefined") {
+                this._workContext.rotate(90/180 * Math.PI);
+                this._workContext.transform(1, 0, 0, -1, 0, 0);
+                this._workContext.fillText(str, 0 , this.fontSize - (this.fontSize/10));
+                this._imageCache[str] = this._workContext.getImageData(0, 0, this.fontSize, this.fontSize);
+                this.resetWorkCanvas();
+            }
+            this._mainContext.putImageData(this._imageCache[str], x, y - (this.fontSize/2));
+        },
+
+        /**
+         * 句読点の位置調節
+         * @param {string} str
+         * @param {number} x
+         * @param {number} y
+         */
+        fillPunctuation: function(str, x, y){
+            this._mainContext.fillText(str, x    + this.fontSize / 2, y - this.fontSize / 2);
+        },
+
+        /**
+         * CanvasのImageDataから文字の塗りつぶし範囲を取得
+         * @param {ImageData} imageData
+         * @returns {object}
+         */
+        getFillExtent: function(imageData){
+            var u8ca = imageData.data;
+            var r, g, b, a;
+            var x = 0, y = 0;
+            var hMargin = this.getMarginMap();
+            var vMargin = this.getMarginMap();
+            var xFound = false;
+
+            for (var i=0,l=u8ca.length;i<l;i+=4) {
+                r = u8ca[i + 0];
+                g = u8ca[i + 1];
+                b = u8ca[i + 2];
+                a = u8ca[i + 3];
+
+                // 現ラインで色があったフラグ
+                if (a !== 0) {
+                    xFound = true;
+                } else {
+                    // debug
+                    imageData.data[i + 3] = 40;
+                }
+
+                // X方向の余白リスト
+                if (hMargin[x] === 0) {
+                    hMargin[x] = (a === 0) ? 0 : 1;
+                }
+
+                // x終端
+                if (x === this.fontSize -1) {
+                    // Y方向の余白リスト
+                    vMargin[y] = (!xFound) ? 0 : 1;
+                    x = 0;
+                    ++ y;
+                    xFound = false;
+                    continue;
+                }
+                ++ x;
+            }
+
+            // 余白情報のオブジェクト
+            var exData = {
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                originWidth: 0,
+                originHeight: 0,
+                verticalMargin: 0,
+                horizontalMargin: 0
+            };
+
+            // 縦
+            for (i = 0, l = vMargin.length; i < l; i++) {
+                if (vMargin[i] === 0) {
+                    if (i < l / 2) { ++ exData.top; }
+                    else { ++ exData.bottom; }
+                }
+            }
+
+            // 横
+            for (i = 0, l = hMargin.length; i < l; i++) {
+                if (hMargin[i] === 0) {
+                    if (i < l / 2) { ++ exData.left; }
+                    else { ++ exData.right; }
+                }
+            }
+
+            exData.originWidth  = imageData.width - exData.left - exData.right;
+            exData.originHeight = imageData.height - exData.top - exData.bottom;
+
+            exData.horizontalMargin = (exData.left + exData.right) / 2;
+            exData.verticalMargin   = (exData.top + exData.bottom) / 2;
+
+            return exData;
+        },
+
+        /**
+         * canvasに描画
+         * @param {Array} charAry
+         */
+        renderCanvas: function(charAry /*, startPint */){
+
+            var startPint = arguments[1]||0;
+            var hSize = this._mainCanvas.width / (this.fontSize + this.lineHeight) | 0;
+            var vSize = this._mainCanvas.height / this.fontSize | 0;
+
+            console.log("hSize:", hSize, "vSize", vSize);
+
+            var x = 0, y = 0;
+            var hCount = 0, vCount = 0, tCount = 0;
+            var chr;
+
+            for (var i = startPint, l = charAry.length; i < l; i++)
+            {
+                ++ tCount;
+
+                chr = charAry[i];
+
+                // 全角以外の空白文字
+                if (this.isSpace(chr)) {
+                    continue;
+                }
+
+                // 改行コードによる強制改行
+                if (this.isLineBreak(chr)) {
+                    vCount = 0;
+                    ++ hCount;
+                    continue;
+                }
+
+                // 文字送り
+                ++ vCount;
+
+                // 行末禁則処理
+                if (vSize -1 === vCount) {
+                    if (this.isNotAllowedEnd(chr)) {
+                        vCount = 1;
+                        ++ hCount;
+                    }
+                }
+
+                // 文字数オーバーによる自然改行
+                if (vSize <= vCount) {
+                    // 行頭禁則処理、連続する場合は無視
+                    if ( !(this.isNotAllowedStart(chr) && vSize === vCount) ) {
+                        vCount = 1;
+                        ++ hCount;
+                    }
+                }
+
+                // ページ終端
+                if (hSize <= hCount) {
+                    break;
+                }
+
+                // 描画座標
+                x = this._mainCanvas.width - (this.fontSize) - (hCount * (this.fontSize + this.lineHeight)) - this.lineHeight;
+                y = this.fontSize * vCount;
+
+                // debug
+                if (this._debug) {
+                    this._mainContext.fillStyle = "rgb(200, 200, 200)";
+                    this._mainContext.fillRect(x, y - this.fontSize, this.fontSize, this.fontSize);
+                    this._mainContext.fillStyle = "rgb(0, 0, 0)";
+                }
+
+                // 文字種毎の特殊対応 ----------
+
+                // 半角英数
+                if (this.isAlphaNum(chr)) {
+                    this.fillAlphaNum(chr, x, y);
+                }
+                // 長音符
+                else if (this.isHyphen(chr)) {
+                    this.fillHyphen(chr ,x, y);
+                }
+                // 始め括弧
+                else if (this.isOpeningBracket(chr)) {
+                    this.fillOpeningBracket(chr, x, y);
+                }
+                // 閉じ括弧
+                else if (this.isClosingBracket(chr)) {
+                    this.fillClosingBracket(chr, x, y);
+                }
+                // 句読点
+                else if (this.isPunctuation(chr)) {
+                    this.fillPunctuation(chr, x, y);
+                }
+                // その他
+                else {
+                    this._mainContext.fillText(chr, x, y);
+                }
+
+            }
+
+            this._lastPoint = tCount -1;
+
+        }
+
+    };
+
+    window.VertiCanvas = VertiCanvas;
+
+})(window);
